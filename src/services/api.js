b9d33ac77dab9b933f64b52549a2add91a47d0c6
Burak Yüce <burak@example.com>
2025-08-26 06:47:55 +0300

    a

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-
 const BASE_URL =
   import.meta.env.VITE_API_URL || "https://rental-backend-eight.vercel.app";
-
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
